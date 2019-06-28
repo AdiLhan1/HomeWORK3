ae24e7872a723bde3997f97b4464c3c9a3794d90
AdiLhan1 <kelsinbekovadilhan2018@gmail.com>
2019-06-28 16:53:32 +0600

    Дополнительное задание

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,42 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         String[] names = {"Коля", "Саша", "Маша"};
+        names=Arrays.copyOf(names,names.length + 1);
+        names[3]="Андрей";
+
         for (String s : names) {
             switch (s) {
                 case "Коля":
                     System.out.println(s + " Good Morning!");
                     break;
                 case "Саша":
-                    System.out.println(s+" Good day!");
+                    System.out.println(s + " Good day!");
                     break;
                 case "Маша":
                     System.out.println(s + " Good Evening!");
+                    break;
+                case "Андрей":
+                    System.out.println(s + " Good buy!");
                     break;
 
             }
 
 
+
+
+
+
+
+
+
+            }
         }
+
+
     }
-}
 
